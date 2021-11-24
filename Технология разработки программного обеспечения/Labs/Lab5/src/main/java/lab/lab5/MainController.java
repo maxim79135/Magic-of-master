8520371f99703d67169f332114584335626ee505
Max <maxim79135@gmail.com>
2021-11-24 09:34:30 +0300

    Complete lab5

--- Технология разработки программного обеспечения/Labs/Lab5/src/main/java/lab/lab5/MainController.java
+++ Технология разработки программного обеспечения/Labs/Lab5/src/main/java/lab/lab5/MainController.java
@@ -1,22 +1,44 @@
 package lab.lab5;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import library.RabinKarp;
+import library.KMT;
+import library.ZFunction;
+
+import java.util.Arrays;
 
 public class MainController {
-    @FXML
-    private Label resultText;
-    @FXML
-    private TextField rabinKarpText1;
-    @FXML
-    private TextField rabinKarpText2;
+    public Label resultText2;
+    public TextField KMTText2;
+    public TextField KMTText1;
+    public Label resultText1;
+    public TextField rabinKarpText1;
+    public TextField rabinKarpText2;
+    public TextField zFunctionText1;
+    public Label resultText3;
 
     @FXML
-    protected void onResult1ButtonClick() {
-        RabinKarp rabinKarp = new RabinKarp(rabinKarpText1.getText(), rabinKarpText2.getText(), 13);
-        String result = rabinKarp.search().toString();
-        resultText.setText(rabinKarp.search().toString());
+    protected void onResult1ButtonClick(ActionEvent event) {
+        Node node = (Node) event.getSource();
+        String data = (String) node.getUserData();
+        switch (data) {
+            case "rabinKarp":
+                RabinKarp rabinKarp = new RabinKarp(rabinKarpText1.getText(), rabinKarpText2.getText(), 13);
+                resultText1.setText(rabinKarp.search().toString());
+                break;
+            case "KMT":
+                KMT kmt = new KMT(KMTText1.getText(), KMTText2.getText());
+                resultText2.setText(kmt.search().toString());
+                break;
+            case "zFunction":
+                ZFunction zFunction = new ZFunction(zFunctionText1.getText());
+                resultText3.setText(Arrays.toString(zFunction.calculate()));
+                break;
+
+        }
     }
 }
